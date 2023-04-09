d8ec21a0ec5fd8c2e2af1422b1560a3fe5f85665
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-09 13:51:40 -0300

    [3C] favorite-tab-creation - feat: created the all and favorites tabs in the list page

--- src/screens/ListScreen/index.tsx
+++ src/screens/ListScreen/index.tsx
@@ -1,24 +1,62 @@
 import PokemonCard from '@components/PokemonCard'
 import React, { useCallback, useEffect, useState } from 'react'
-import { Container, Flex, Spinner } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Flex,
+  Spinner,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react'
 import { useSearchParams } from 'react-router-dom'
 import PaginationButtons from '@components/PaginationButtons'
+import { useSelector } from 'react-redux'
+import { selectFavoritesState } from '@store/favoritesSlice'
 
 function ListScreen() {
-  const [searchParams] = useSearchParams()
+  const ITEMS_PER_PAGE = 10
+
+  const favoritesState = useSelector(selectFavoritesState)
+
+  const [searchParams, setSearchParams] = useSearchParams()
 
   const [pokemonPrevious, setPokemonPrevious] = useState<string | null>(null)
   const [pokemonList, setPokemonList] = useState<any[]>([])
   const [pokemonNext, setPokemonNext] = useState<string | null>(null)
 
+  const [favoritePokemonPrevious, setFavoritePokemonPrevious] = useState<
+    string | null
+  >(null)
+  const [favoritePokemonList, setFavoritePokemonList] = useState<any[]>([])
+  const [favoritePokemonNext, setFavoritePokemonNext] = useState<string | null>(
+    null,
+  )
+
   const [isPokemonListLoading, setIsPokemonListLoading] =
+    useState<boolean>(true)
+
+  const [isFavoritePokemonListLoading, setIsFavoritePokemonListLoading] =
     useState<boolean>(true)
 
   const handleGetPokemon = useCallback(
     async (baseUrl = `${process.env.NEXT_PUBLIC_POKEMON_API_URI}`) => {
-      const page = Number(searchParams.get('page') ?? 1)
+      let page = 1
 
-      const url = `${baseUrl}?offset=${(page - 1) * 10}&limit=10`
+      if (searchParams.get('page')) {
+        if (Number(searchParams.get('page')) <= 0) {
+          page = 1
+          searchParams.set('page', String(page))
+          setSearchParams(searchParams)
+        } else {
+          page = Number(searchParams.get('page'))
+        }
+      }
+
+      const url = `${baseUrl}?offset=${(page - 1) * 10}&limit=${ITEMS_PER_PAGE}`
 
       await fetch(url)
         .then((res) => res.json())
@@ -29,8 +67,57 @@ function ListScreen() {
         })
         .catch((error) => console.error(error))
     },
-    [searchParams, setPokemonPrevious, setPokemonList, setPokemonNext],
+    [
+      searchParams,
+      setSearchParams,
+      setPokemonPrevious,
+      setPokemonList,
+      setPokemonNext,
+    ],
   )
+
+  const handleGetFavoritePokemon = useCallback(() => {
+    let favoritesPage = 1
+
+    if (searchParams.get('favoritesPage')) {
+      if (Number(searchParams.get('favoritesPage')) <= 0) {
+        favoritesPage = 1
+        searchParams.set('favoritesPage', String(favoritesPage))
+        setSearchParams(searchParams)
+      } else {
+        favoritesPage = Number(searchParams.get('favoritesPage'))
+      }
+    }
+
+    const startIndex = (favoritesPage - 1) * ITEMS_PER_PAGE
+    const endIndex = startIndex + ITEMS_PER_PAGE
+
+    const favorites = favoritesState
+      .slice(startIndex, endIndex)
+      .map((pokemonID) => ({
+        url: `${process.env.NEXT_PUBLIC_POKEMON_API_URI}${pokemonID}`,
+      }))
+
+    const data = {
+      results: favorites,
+    }
+
+    setFavoritePokemonPrevious(
+      !!startIndex === true ? String(!!startIndex) : null,
+    )
+    setFavoritePokemonList(data?.results)
+    setFavoritePokemonNext(
+      endIndex < favoritesState?.length === true
+        ? String(endIndex > favoritesState?.length)
+        : null,
+    )
+  }, [searchParams, setSearchParams, favoritesState, setFavoritePokemonList])
+
+  useEffect(() => {
+    searchParams.set('page', '1')
+    searchParams.set('favoritesPage', '1')
+    setSearchParams(searchParams)
+  }, [])
 
   useEffect(() => {
     ;(async () => {
@@ -38,7 +125,15 @@ function ListScreen() {
       await handleGetPokemon()
       setIsPokemonListLoading(false)
     })()
-  }, [searchParams])
+  }, [searchParams, handleGetPokemon])
+
+  useEffect(() => {
+    ;(async () => {
+      setIsFavoritePokemonListLoading(true)
+      await handleGetFavoritePokemon()
+      setIsFavoritePokemonListLoading(false)
+    })()
+  }, [favoritesState, searchParams, handleGetFavoritePokemon])
 
   return (
     <Container
@@ -47,33 +142,86 @@ function ListScreen() {
       boxSizing="border-box"
       centerContent
     >
-      {isPokemonListLoading ? (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="#3c5aa6"
-          size="xl"
-        />
-      ) : (
-        <>
-          <PaginationButtons
-            handleGetPokemon={handleGetPokemon}
-            pokemonPrevious={pokemonPrevious}
-            pokemonNext={pokemonNext}
-          />
-          <Flex wrap="wrap" justifyContent="space-around">
-            {(pokemonList ?? [])?.map((pokemon, key) => (
-              <PokemonCard key={key} pokemon={pokemon} />
-            ))}
-          </Flex>
-          <PaginationButtons
-            handleGetPokemon={handleGetPokemon}
-            pokemonPrevious={pokemonPrevious}
-            pokemonNext={pokemonNext}
-          />
-        </>
-      )}
+      <Tabs width="100%">
+        <TabList>
+          <Tab>Todos</Tab>
+          <Tab>Favoritos</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            {isPokemonListLoading ? (
+              <Box width="100%" display="flex" justifyContent="center">
+                <Spinner
+                  thickness="4px"
+                  speed="0.65s"
+                  emptyColor="gray.200"
+                  color="#3c5aa6"
+                  size="xl"
+                />
+              </Box>
+            ) : (
+              <>
+                <PaginationButtons
+                  handleGetPokemon={handleGetPokemon}
+                  pokemonPrevious={pokemonPrevious}
+                  pokemonNext={pokemonNext}
+                  searchParamName="page"
+                />
+                <Flex wrap="wrap" justifyContent="space-around">
+                  {(pokemonList ?? [])?.map((pokemon, key) => (
+                    <PokemonCard key={key} pokemon={pokemon} />
+                  ))}
+                </Flex>
+                <PaginationButtons
+                  handleGetPokemon={handleGetPokemon}
+                  pokemonPrevious={pokemonPrevious}
+                  pokemonNext={pokemonNext}
+                  searchParamName="page"
+                />
+              </>
+            )}
+          </TabPanel>
+          <TabPanel>
+            {isFavoritePokemonListLoading ? (
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="#3c5aa6"
+                size="xl"
+              />
+            ) : (
+              <>
+                {favoritesState?.length > 10 && (
+                  <PaginationButtons
+                    handleGetPokemon={handleGetFavoritePokemon}
+                    pokemonPrevious={favoritePokemonPrevious}
+                    pokemonNext={favoritePokemonNext}
+                    searchParamName="favoritesPage"
+                  />
+                )}
+                <Flex wrap="wrap" justifyContent="space-around">
+                  {favoritePokemonList?.length ? (
+                    (favoritePokemonList ?? [])?.map((pokemon, key) => (
+                      <PokemonCard key={key} pokemon={pokemon} />
+                    ))
+                  ) : (
+                    <Text>No favorite pokemon</Text>
+                  )}
+                </Flex>
+                {favoritesState?.length > 10 && (
+                  <PaginationButtons
+                    handleGetPokemon={handleGetFavoritePokemon}
+                    pokemonPrevious={favoritePokemonPrevious}
+                    pokemonNext={favoritePokemonNext}
+                    searchParamName="favoritesPage"
+                  />
+                )}
+              </>
+            )}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Container>
   )
 }
