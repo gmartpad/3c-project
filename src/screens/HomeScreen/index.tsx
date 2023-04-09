9f1af9c3bea7649d5cb2b417ce9792731a5e6c7b
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-09 09:42:05 -0300

    [3C] navbar-creation - feat: created navbar (consisting of the header and sidebar components)

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,19 +1,32 @@
-import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HomeScreen() {
+  const isBelowMD = useBreakpointValue({
+    base: true,
+    sm: true,
+    md: false,
+  })
+
   return (
     <Container
       minHeight="100vh"
       display="flex"
       justifyContent="center"
       alignItems="center"
+      flexDirection={isBelowMD ? 'column' : 'row'}
     >
-      <Heading as="h1" size="2xl">
+      <Heading textAlign={isBelowMD ? 'center' : 'inherit'} as="h1" size="2xl">
         My Favorite Pokemon
       </Heading>
-      <Box display="flex" flexDirection="column" alignItems="center">
+      <Box mt={4} display="flex" flexDirection="column" alignItems="center">
         <Text fontSize="xl" mb={4} marginInline="8" textAlign="center">
           Welcome to My Awesome App! Check out our list of Pokemons below:
         </Text>
